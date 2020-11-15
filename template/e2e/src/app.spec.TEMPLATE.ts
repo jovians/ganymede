ec99ef1ccba8e78b25e095137e18e408ad262119
Jovian <jovian.technologies@gmail.com>
2020-11-14 23:36:08 -0800

    replacer prefix

--- template/e2e/src/app.spec.TEMPLATE.ts
+++ template/e2e/src/app.spec.TEMPLATE.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     page.navigateTo();
-    expect(await page.getTitleText()).toEqual('<ANGULAR_APP_NAME> app is running!');
+    expect(await page.getTitleText()).toEqual('<gany.ANGULAR_APP_NAME> app is running!');
   });
 
   afterEach(async () => {
