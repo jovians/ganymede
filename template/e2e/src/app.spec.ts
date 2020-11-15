7309b6034f08b2da2f7ca180dbd005b9dd66734c
Jovian <jovian.technologies@gmail.com>
2020-11-14 17:35:34 -0800

    update

--- template/e2e/src/app.spec.ts
+++ template/e2e/src/app.spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     page.navigateTo();
-    expect(await page.getTitleText()).toEqual('ganymede-clr app is running!');
+    expect(await page.getTitleText()).toEqual('<ANGULAR_APP_NAME> app is running!');
   });
 
   afterEach(async () => {
